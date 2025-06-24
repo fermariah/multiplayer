218a938b7905e395a5e8d9524bad4b909489135d
fermariah <mariah.fe.santos.soares@gmail.com>
2025-06-24 13:44:15 -0300

    codigo
    está sendo desenvolvido um código para o lobby, e até o momento está funcionando a parte de gerar um código específico pra entrar na sala do lobby

--- multiplayer/Assets/LobbyManager.cs
+++ multiplayer/Assets/LobbyManager.cs
@@ -3,10 +3,14 @@ using Unity.Services.Core;
 using Unity.Services.Authentication;
 using Unity.Services.Lobbies.Models;
 using Unity.Services.Lobbies;
+using System.Threading.Tasks;
+using UnityEditor;
+using TMPro;
 
 
 public class LobbyManager : MonoBehaviour
 {
+    public TMP_InputField namePLAYERinput, CODEinput;
     Lobby hostLobby, joinnedLobby;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     async void Start()
@@ -14,17 +18,25 @@ public class LobbyManager : MonoBehaviour
         await UnityServices.InitializeAsync();
     }
 
-   async void Authenticate()
+   async Task Authenticate()
     {
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
+        Debug.Log("Usuário logado como: " + AuthenticationService.Instance.PlayerId);
 
     }
 
     async public void CreateLobby()
     {
-        Authenticate();
+        await Authenticate();
         hostLobby = await Lobbies.Instance.CreateLobbyAsync("lobby", 4);
+        Debug.Log("Criou o lobby " + hostLobby.LobbyCode);
     }
 
+    async public void JoinLobbyByCode()
+    {
+        await Authenticate();
+        joinnedLobby = await Lobbies.Instance.JoinLobbyByCodeAsync(CODEinput.text);
+        Debug.Log("Entrou no lobby " + joinnedLobby.LobbyCode);
+    }
 
 }
