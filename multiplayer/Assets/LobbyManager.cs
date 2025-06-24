4071ddab2ffed123c8ae35119fa5591659c1cdc6
fermariah <mariah.fe.santos.soares@gmail.com>
2025-06-24 14:28:22 -0300

    problema 2
    deu problema da parte do script, onde ta a area de "Data", acredita-se que é por conta da nova atualização da unity

--- multiplayer/Assets/LobbyManager.cs
+++ multiplayer/Assets/LobbyManager.cs
@@ -6,6 +6,7 @@ using Unity.Services.Lobbies;
 using System.Threading.Tasks;
 using UnityEditor;
 using TMPro;
+using System.Collections.Generic;
 
 
 public class LobbyManager : MonoBehaviour
@@ -20,6 +21,10 @@ public class LobbyManager : MonoBehaviour
 
    async Task Authenticate()
     {
+        if (AuthenticationService.Instance.IsSignedIn)
+            return;
+
+        AuthenticationService.Instance.ClearSessionToken();
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
         Debug.Log("Usuário logado como: " + AuthenticationService.Instance.PlayerId);
 
@@ -28,15 +33,69 @@ public class LobbyManager : MonoBehaviour
     async public void CreateLobby()
     {
         await Authenticate();
-        hostLobby = await Lobbies.Instance.CreateLobbyAsync("lobby", 4);
+
+        CreateLobbyOptions options = new CreateLobbyOptions
+        {
+            Player = GetPlayer()
+        };
+
+        hostLobby = await Lobbies.Instance.CreateLobbyAsync("lobby", 4, options);
+        joinnedLobby = hostLobby;
         Debug.Log("Criou o lobby " + hostLobby.LobbyCode);
+        InvokeRepeating("SendLobbyHeartBeat", 10, 10);
+        ShowPlayers();
     }
 
     async public void JoinLobbyByCode()
     {
         await Authenticate();
+
+        JoinLobbyByCodeOptions options = new JoinLobbyByCodeOptions
+        {
+            Player = GetPlayer()
+        };
+
+
         joinnedLobby = await Lobbies.Instance.JoinLobbyByCodeAsync(CODEinput.text);
         Debug.Log("Entrou no lobby " + joinnedLobby.LobbyCode);
     }
 
+    Player GetPlayer()
+    {
+        Player player = new Player();
+        {
+            Data = new Dictionary<string, PlayerDataObject>
+            {
+                { "name", new PlayerDataObject(PlayerDataObject.VisibilityOptions.Public, namePLAYERinput.text) }
+            }
+        };
+
+        return player;
+    }
+    async void SendLobbyHeartBeat()
+    {
+        if (hostLobby == null)
+            return;
+
+        await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+        Debug.Log("Atualizou o lobby");
+        UptadeLobby();
+        ShowPlayers();
+    }
+
+    void ShowPlayers()
+    {
+        for (int i = 0; i < joinnedLobby.Players.Count; i++)
+        {
+            Debug.Log(joinnedLobby.Players[i].Data["name"].Value);
+        }
+    }
+
+    async void UptadeLobby()
+    {
+        if (joinnedLobby == null)
+            return;
+
+        joinnedLobby = await LobbyService.Instance.GetLobbyAsync(joinnedLobby.Id);
+    }
 }
